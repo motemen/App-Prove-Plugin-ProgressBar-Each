d4bbfc3c96bdc80c5816b66f1a8d5e1631ad0c8c
motemen <motemen@gmail.com>
2010-07-05 20:48:33 +0900

    initial commit

--- lib/App/Prove/Plugin/ProgressBar/Each.pm
+++ lib/App/Prove/Plugin/ProgressBar/Each.pm
@@ -1,9 +1,76 @@
 package App::Prove::Plugin::ProgressBar::Each;
 use strict;
 use warnings;
+
 our $VERSION = '0.01';
 
+sub load {
+    my ($class, $p) = @_;
+    $p->{app_prove}->formatter('App::Prove::Plugin::ProgressBar::Formatter');
+    $p->{app_prove}->merge(1);
+    return 1;
+}
+
+{
+    package App::Prove::Plugin::ProgressBar::Formatter;
+    use base 'TAP::Formatter::Console';
+    use Term::ProgressBar::Simple;
+
+    sub new {
+        my ($class, $args) = @_;
+        $args->{verbosity} = -2;
+        return $class->SUPER::new($args);
+    }
+
+    sub open_test {
+        my ($self, $test, $parser) = @_;
+
+        print "\n" if exists $self->{progress};
+        $self->_set_colors('green');
+
+        my $name = sprintf '%-25s', "$test";
+        if (length $name > 25) {
+            $name = '...' . substr $name, -22;
+        }
+        my $progress = Term::ProgressBar::Simple->new({
+            name  => $name,
+            count => 1000,
+        });
+        $self->{progress} = $progress;
+
+        $parser->callback(
+            EOF => sub {
+                undef $self->{progress};
+                $self->_set_colors('reset');
+            }
+        );
+
+        $parser->callback(
+            plan => sub {
+                my $plan = shift;
+                $self->{progress}->{tpq}->target($plan->tests_planned);
+                $self->{progress}->{args}->{count} = $plan->tests_planned;
+            }
+        );
+
+        $parser->callback(
+            test => sub {
+                my $test = shift;
+                if (not $test->is_ok) {
+                    print "\r", (' ' x $self->{progress}->{tpq}->term_width);
+                    $self->_set_colors('red')
+                }
+                $self->{progress}++;
+                undef;
+            }
+        );
+
+        $self->SUPER::open_test($test, $parser);
+    }
+}
+
 1;
+
 __END__
 
 =head1 NAME
